28c96b1365a67365334f8f6ff8e8d539d9ef3db7
Fauzi Al Aziz <alaziz.fauzi@gmail.com>
2019-12-14 16:57:12 +0700

    Working copy text and basic effect.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, {useEffect, useState, useRef} from 'react';
 import './App.scss';
+import Emoji from './components/Emoji'
 import localforage from 'localforage'
 import LOCAL from './config';
 import { Obj } from './services'
-// import {CopyToClipboard} from 'react-copy-to-clipboard'
+import {CopyToClipboard} from 'react-copy-to-clipboard'
+
+function ModelForm() {
+  this.id = ''
+  this.title = ''
+  this.contents = [
+    { id : '', text : '', copied : false, deleted : false }
+  ]
+  this.tags = []
+  this.deleted = false
+}
 
 function App() {
   const [data, setData] = useState([])
   const [openModal, setOpenModal] = useState(false)
-  const [form, setForm] = useState({
-    id : '',
-    title : '',
-    contents : [
-      { id : '', text : '', copied : false }
-    ],
-    tags : [],
-  })
+  const [form, setForm] = useState(new ModelForm())
   const ref = useRef(null)
 
   useEffect(() => {
@@ -29,20 +33,6 @@ function App() {
      console.log(err)
     }
   }, [])
-
-  // const getData = () => localforage.getItem(dbName).then(value => {
-  //   setData(value)
-  // })
-
-  // const onCopy = key => {
-  //   let temp = data
-  //   for(let item of temp) {
-  //     if(item.ssn === key) {
-  //       item.copy = true
-  //     } else item.copy = false
-  //   }
-  //   setData(JSON.parse(JSON.stringify(temp)))
-  // }
 
   const modalOpen = () => {
     setOpenModal(true)
@@ -75,28 +65,66 @@ function App() {
     setForm(Obj.deepCopy(temp))
   }
 
+  const onClose = () => {
+    setOpenModal(false)
+    setForm(new ModelForm())
+  }
+
   const onSubmit = () => {
     let temp = data
+    if(form.contents[0].text === "") {
+      onClose()
+      return
+    }
     temp.push(form)
     localforage.setItem(LOCAL.tableName, temp).then( res => {
       console.log(res)
       setData(res)
-      setOpenModal(false)
+      onClose()
     })
+  }
+
+  const onCopy = (id, index) => {
+    let temp = data
+    // temp[index].contents.forEach( (content, i) => {
+    //   if(content.id === id) {
+      //   }
+      // })
+    temp[index].contents[0].copied = true
+    setData(Obj.deepCopy(temp))
+    setTimeout(() => {
+      temp[index].contents[0].copied = false
+      setData(Obj.deepCopy(temp))
+    }, 1300);
   }
 
   return (
     <div className="outer-wrapper">
       <div className="app-frame">
-        <button className="add-button" onClick={modalOpen}>+</button>
+        <div className="list-wrapper">
 
-        {data.map(e => (
-          <div>{e.contents[0].text}</div>
-        ))}
+          {data.length === 0?
+            <div className="on-list-empty">{LOCAL.onListEmpty}<br/><Emoji /></div>
+          : data.map((e, i) => (
+            <div className="list-tile" key={e.id}>
+              {e.title?<div className="list-title">{e.title}</div>: null}
+              <div className="list-content">{e.contents[0].text}
+                <div className={e.contents[0].copied? "copy-sign-on" : "copy-sign-off"}>Copied to clipboard!</div>
+                <div className="list-boundary-line" />
+                <CopyToClipboard text={e.contents[0].text} onCopy={() => onCopy(e.contents[0].id, i)}>
+                  <button className="list-button">Copy</button>
+                </CopyToClipboard>
+              </div>
+            </div>
+          ))}
+
+        </div>
+
+        <button className="add-button" onClick={modalOpen}>+</button>
 
         <div className={openModal? "modal-open" : "modal-close"}>
           <div className="modal-wrapper">
-            <button className="modal-left-btn" onClick={() => setOpenModal(!openModal)}>Cancel</button>
+            <button className="modal-left-btn" onClick={onClose}>Cancel</button>
             <button className="modal-right-btn" onClick={onSubmit}>Save</button>
             
             <input className="modal-title" placeholder="Title (optional)" value={form.title} onChange={onChangeTitle} />
--- src/config/constant.js
+++ src/config/constant.js
@@ -1,6 +1,7 @@
 const LOCAL = {
     tableName : 'userDataList',
-    initPackId : 'DL0001'
+    initPackId : 'DL0001',
+    onListEmpty : "It seems that you don't have data to copy. Please add some."
 }
 
 export default LOCAL
