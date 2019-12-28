e46c7d2049b4e147bd907fef50abef0d16118910
Fauzi Al Aziz <alaziz.fauzi@gmail.com>
2019-12-28 16:49:03 +0700

    Multiple Text input layout.

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -71,34 +71,48 @@ const Modal = forwardRef(({openModal, onSubmit, onClose, form, onChangeTitle, on
                     <div className="multipletext-slot">{
                         form.contents.map( (content, i) => (
                             <div className={content.withInfo? "multipletext-list-withInfo" : "multipletext-list"} key={i}>
+                                <div className="multipletext-arrow">
+                                    <FontAwesomeIcon icon="caret-right" />
+                                </div>
+
+                                {/* CONTENT INPUT */}
                                 <input className="multipletext-text" autoFocus
                                     name={i + "-text"}
                                     value={content.text}
                                     onChange={onChangeText}
                                 />
+
+                                {/* ADD INFO BUTTON */}
                                 <button className={content.withInfo? "multipletext-addinfo-on" : "multipletext-addinfo-off"} onClick={() => infoSwitch(i)}>
                                     <FontAwesomeIcon icon="comment-dots" />
                                 </button>
+
+                                {/* CLOSE CONTENT BUTTON */}
                                 <button className="multipletext-close" onClick={() => closeList(i)}>
                                     <FontAwesomeIcon icon="times" />
                                 </button>
+
+                                {/* CONTENT INFO INPUT */}
                                 <div className="info-base">
                                     <div className="info-relative">
                                         <div className="info-pipe" />
-                                        <input className="info-text" placeholder="Short comment"
+                                        <input className="info-text" placeholder="short comment"
                                             name={i + "-info"}
                                             value={content.info}
                                             onChange={onChangeText}
                                         />
                                     </div>
                                 </div>
+
                             </div>
                         ))
                     }
                         {showAdd?
+
+                        /* ADD CONTENT BUTTON */
                         <button className="mutlipletext-add" onClick={onAddList}>
                             <FontAwesomeIcon icon="plus" />
-                            New Text
+                            <div>New Text</div>
                         </button>
                         :null}
                     </div>
