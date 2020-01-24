fd11fa7c677e2a66b5c91cf4ba316b8498e2674a
Fauzi Al Aziz <alaziz.fauzi@gmail.com>
2020-01-24 23:48:47 +0700

    Delete Model.

--- src/model/index.js
+++ src/model/index.js
@@ -1,11 +1,7 @@
-import ModelContent from "./ModelContent";
-import ModelForm from "./ModelForm";
 import makeContent from "./MakeContent";
 import makeForm from "./MakeForm";
 
 export {
     makeForm,
-    makeContent,
-    ModelContent,
-    ModelForm
+    makeContent
 }
