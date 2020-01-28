4fb33eaa10ebd7f9c35143b790d66292bf03a34f
Fauzi Al Aziz <alaziz.fauzi@gmail.com>
2020-01-29 01:21:31 +0700

    Add minor style change when search focus.

--- src/components/headbar/Headbar.jsx
+++ src/components/headbar/Headbar.jsx
@@ -2,16 +2,30 @@
 import React from 'react'
 import './Headbar.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from 'react'
 
 const Headbar = (props) => {
+    const [searchFocus, setSearchFocus] = useState(false)
+    const [searchText, setSearchText] = useState('')
+
+    const headbarButtonHandler = () => {
+        if(searchFocus) {
+            setSearchText('')
+            setSearchFocus(false)
+        }
+    }
     return (
         <div className="headbar-base">
-            <div className="headbar-frame">
-                <button className="headbar-btn">
-                    <FontAwesomeIcon icon="bars" />
+            <div className={searchFocus? "headbar-frame-focus" : "headbar-frame"}>
+                <button className="headbar-btn" onClick={headbarButtonHandler}>
+                    <FontAwesomeIcon icon={searchFocus? "arrow-left" : "bars"} />
                 </button>
-                <input className="headbar-search"
+                <input className="headbar-search" 
+                    onFocus={() => setSearchFocus(true)}
+                    // onBlur={() => setSearchFocus(false)}
                     placeholder="Search your text"
+                    value={searchText}
+                    onChange={e => setSearchText(e.target.value)}
                 />
             </div>   
         </div>
