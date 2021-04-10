e297be05819e4278774bf4cfdd7c5d1be9c5e362
Fauzi Al Aziz <alaziz.fauzi@gmail.com>
2021-04-11 03:15:41 +0700

    OCR Prototype

---
+++ src/components/modal-image/index.jsx
@@ -0,0 +1,140 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useEffect, useRef, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import Tesseract, { createWorker } from 'tesseract.js'
+
+const ModalImage = () => {
+    const [openModal, setOpenModal] = useState(false)
+    const [openOptions, setOpenOptions] = useState(false)
+    const [ocrResult, setOcrResult] = useState('')
+    const history = useHistory()
+    const mount = useRef(false)
+
+    const inputRef = useRef()
+
+    useEffect(() => {
+        mount.current = true
+        setOpenModal(true)
+        return () => {
+            mount.current = false
+        }
+    }, [])
+
+    const worker = createWorker({
+        logger: m => {
+            console.log(m)
+            if (m?.jobId) {
+                const progress = Number(m.progress)
+                const message = `recognizing... (${progress.toFixed(2) * 100}%)`
+                setOcrResult(message)
+            }
+        },
+    })
+
+    const runOCR = async (scr) => {
+        await worker.load()
+        await worker.loadLanguage('eng')
+        await worker.initialize('eng')
+        const { data: { text } } = await worker.recognize(scr)
+        if (!mount.current) {
+            console.log('component unmounted')
+            return
+        }
+        setOcrResult(text)
+    }
+
+    /*  To make the user see that
+        options was closed first
+        before modal close   */
+    const checkOptions = fun => {
+        if(openOptions) {
+            setOpenOptions(false)
+            setTimeout(() => {
+                fun()
+            }, 200);
+        }else fun()
+    }
+
+    const onCloseHandler = () => {
+        setOpenModal(false)
+        // onClose()
+        setTimeout(() => {
+            history.replace('/')
+            // dispatch(clearForm())
+        }, 200);
+    }
+
+    const readFile = (e) => {
+        if (e.target.files) {
+            const file = e.target.files[0];
+            // const allowedExt = ['image/png', 'image/jpg', 'image/jpeg'];
+            // debugger
+            // if (!allowedExt.includes(file.type)) return;
+
+            const reader = new FileReader();
+
+            reader.fileName = file.name
+            reader.onload = e => {
+                if (e.target) {
+                    const url = e.target.result
+                    if(!url || (typeof url !== 'string')) return
+                    
+                    let image = new Image()
+                    image.addEventListener('load', (loadedImage) => {
+                        let url = loadedImage.srcElement.src
+                        setOcrResult('preparing...')
+                        runOCR(url)
+                    })
+                    image.src = url
+                }
+            };
+            // debugger
+            reader.readAsDataURL(file);
+        }
+    }
+
+    return (
+        <div className={openModal? "modal-open" : "modal-close"}>
+            <div className="modal-wrapper">
+                {/* MODAL HEADER SECTION */}
+                <div className="modal-header">
+
+                    {/* BACK BUTTON */}
+                    <button
+                        className="modal-back-btn"
+                        onClick={() => checkOptions(onCloseHandler)} >
+                        <FontAwesomeIcon icon="arrow-left" />
+                    </button>
+
+                </div>
+
+                <div style={{padding: 20}}>
+                    {ocrResult}
+                </div>
+
+                <div style={{position: 'fixed', bottom: 0, padding: 10}}>
+                    <button
+                        style={{ color: 'white', }}
+                        onClick={() => {
+                            if (inputRef.current) {
+                                inputRef.current.value = '';
+                                inputRef.current.click();
+                            }
+                        }}>
+                        select image
+                    </button>
+                </div>
+            </div>
+
+            <input
+                ref={inputRef}
+                type='file'
+                style={{ display: 'none'}}
+                onChange={readFile}
+                accept='image/*'
+            />
+        </div>
+    )
+}
+
+export default ModalImage
--- src/services/ServiceObject.js
+++ src/services/ServiceObject.js
@@ -1,4 +1,4 @@
-export default {
+const Obj = {
     isEmpty: (obj) => Object.entries(obj).length === 0 && obj.constructor === Object,
     deleteProps: (obj, propNames) => {
         if(propNames.constructor !== Array) throw new Error('Wrong argument type. propNames not an array!')
@@ -10,3 +10,5 @@ export default {
         return obj
     }
 }
+
+export default Obj
